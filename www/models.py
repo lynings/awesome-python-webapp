3295d3e6a4d8f51a722de5e8a486582bd248897f
lynings <645464704@qq.com>
2015-11-29 23:27:31 +0800

    add orm.py and models.py

---
+++ www/models.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+#models.py
+
+__author__ = 'lynings'
+
+import time, uuid
+
+from orm import Model, StringField, BooleanField, FloatField, TextField
+
+def next_id():
+	return '%015d%s000' % (int(time.time() * 1000), uuid.uuid4().hex)
+
+class User(Model):
+	__table__ = 'users'
+
+	id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+	email = StringField(ddl='varchar(50)')
+	passwd = StringField(ddl='varchar(50)')
+	admin = BooleanField()
+	name = StringField(ddl='varcahr(50)')
+	image = StringField(ddl='varchat(500)')
+	create_time = FloatField(default=time.time)
+
+class Bolg(Model):
+	__table__ = 'blogs'
+
+	id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+	blog_id = StringField(ddl='varchar(50)')
+	user_id = StringField(ddl='varchar(50)')
+	user_name = StringField(ddl='varchar(50)')
+	user_image = StringField(ddl='varchar(500)')
+	name = StringField(ddl='varchar(50)')
+	summary = StringField(ddl='varchar(200)')
+	content = TextField()
+	create_time = FloatField(default=time.time)
+
+class Comment(Model):
+	__table__ = 'comments'
+
+	id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+	blog_id = StringField(ddl='varchar(50)')
+	user_id = StringField(ddl='varchar(50)')
+	user_name = StringField(ddl='varchar(50)')
+	user_image = StringField(ddl='varchar(500)')
+	content = TextField()
+	create_time = FloatField(default=time.time)
+
+
+
+	
---
+++ www/orm.py
@@ -0,0 +1,246 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+#orm.py
+
+__author__ = 'lynings'
+
+import asyncio, logging
+import aiomysql
+
+def log(sql, args=()):
+	logging.info('SQL: %s' % sql)
+
+@asyncio.coroutine
+def create_pool(loop, **kw):
+	logging.info('create database connection pool....')
+	global __pool
+	__pool = yield from aiomysql.create_pool(
+		host=kw.get('host', 'localhost'),
+		port=kw.get('port', 3306),
+		user=kw.get('user'),
+		password=kw.get('password'),
+		charset=kw.get('charset', 'utf8'),
+		autocommit=kw.get('autocommit', True),
+		maxsize=kw.get('maxsize', 10),
+		minsize=kw.get('minsize', 1),
+		loop=loop
+	)
+
+@asyncio.coroutine
+def select(sql, args, size=None):
+	log(sql, args)
+	global __pool
+	with (yield from __pool) as conn:
+		cur = yield from conn.cursor(aiomysql.DictCursor)
+		yield from cur.execute(sql.replace('?', '%s'), args or ())
+		if size:
+			rs = yield from cur.fetchmany(size)
+		else:
+			rs = yield from cur.fetchall()
+		yield from cur.close()
+		logging.info('rows return %s' % len(rs))
+		return rs
+
+@asyncio.coroutine
+def execute(sql, args):
+	log(sql)
+	with (yield from __pool) as conn:
+		try:
+			cur = yield from conn.cursor()
+			yield from cur.execute(sql.replace('?', '%s'), args)
+			affected = cur.rowcount
+			yield from cur.close()
+		except BaseException as e:
+			raise
+		return affected
+
+def create_args_string(num):
+	L = []
+	for n in range(num):
+		L.append('?')
+	return ', '.join(L)
+
+class Field(object):
+
+	def __init__(self, name, column_type, primary_key, default):
+		self.name = name
+		self.column_type = column_type
+		self.primary_key = primary_key
+		self.default = default
+
+	def __str__(self):
+		return '<%s, %s:%s>' % (self.__class__.__name__, self.column_type, self.name)
+
+
+class StringField(Field):
+
+	def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
+		super().__init__(name, ddl, primary_key, default)
+
+
+class IntegerField(Field):
+
+	def __init__(self, name=None, primary_key=False, default=0):
+		super().__init__(name, 'bigint', primary_key, default)
+
+
+class BooleanField(Field):	
+
+	def __init__(self, name=None,  default=False):
+		super().__init__(name, 'boolean', False, default)
+
+
+class FloatField(Field):
+
+	def __init__(self, name=None, primary_key=False, default=0.0):
+		super().__init__(name, 'real', primary_key, default)
+
+
+class TextField(Field):
+
+	def __init__(self, name=None, default=None):
+		super().__init__(name, 'text', False, default)
+
+
+class ModelMetaclass(type):
+
+	def __new__(cls, name, bases, attrs):
+		# Exclude "Model"
+		if name=='Model':
+			return type.__new__(cls, name, bases, attrs)
+		# get table name
+		tableName = attrs.get('__table__', None) or name
+		logging.info('found model: %s (table: %s)' % (name, tableName))
+		# get all Field and primarl key
+		mappings = dict()
+		fields = []
+		primary_key = None
+		for k, v in attrs.items():
+			if isinstance(v, Field):
+				logging.info('found mapping: %s ====> %s' % (k, v))	
+				mappings[k] = v
+				if v.primary_key:
+					# found primark key
+					if primary_key:
+						raise RuntimeError('Duplicate primark key for field: %s' % primary_key)
+					primary_key = k
+				else:
+					fields.append(k)
+		if not primary_key:
+			raise RuntimeError('primark key no found.')
+		for k in mappings.keys():
+			attrs.pop(k)
+		escaped_fields = list(map(lambda f: '"%s"' % f, fields))
+		attrs['__mappings__']=mappings #save attribute and row mapping relation
+		attrs['__table__']=tableName 
+		attrs['primary_key']=primary_key #primary key
+		attrs['__fields__']=fields # All attribute, beside primark key 
+
+		# create default for INSERT, DELETE, SELECT, UPDATE sql
+		attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (tableName,','.join(escaped_fields), primary_key, create_args_string(len(escaped_fields)+1))
+		attrs['__delete__'] = 'delete from `%s` where %s = ?' % (tableName, primary_key)
+		attrs['__select__'] = 'select `%s`, %s from `%s`' % (primary_key,','.join(escaped_fields), tableName)
+		attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (tableName,','.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f),fields)), primary_key)
+		return type.__new__(cls, name, bases, 	attrs)
+
+class Model(dict, metaclass=ModelMetaclass):
+
+	def __init__(self, **kw):
+		super(Model, self).__init__(**kw)
+
+	def __getattr__(self, key):
+		try:
+			return self[key]
+		except KeyError:
+			raise AttributeError(r'"Model" object has no attribute %s' % key)
+
+		def __setattr__(self, key, value):
+			self[key] = value
+
+		def getVlaue(self, key):
+			return getattr(self, key, None)
+
+		def getValueOrDefault(self, key):
+			value = getattr(self, key, None)
+			if value is None:
+				value = self.__mappings__[key]
+				if field.default is not None:
+					value = field.default() if callable(field.default) else field.default
+					logging.debug('using default value for %s: %s' % (key, str(value)))
+					setattr(self, key, value)
+			return value
+
+
+	@classmethod
+	@asyncio.coroutine
+	def findAll(cls, where=None, args=None, **kw):
+		'find object by where clause.'
+		sql = [cls.__select__]
+		if where:
+			sql.append('where')
+			sql.append(where)
+		if args is None:
+			args = []
+		orderBy = kw.get('orderBy', None)
+		if orderBy:
+			sql.append('orderBy')
+			sql.append(orderBy)
+		limit = kw.get('limit', None)
+		if limit is not None:
+			sql.append('limit')
+			if isinstance(limit, int):
+				sql.append('?')
+				args.append(limit)
+			elif isinstance(limit, tuple):
+				sql.append('?', '?')
+				args.execute(limit)
+			else:
+				raise ValueError('Invalid limit value: %s' % str(limit))
+		rs = yield from select(' '.join(sql), args)
+		return [cls(**r) for r in rs]
+
+	@classmethod
+	@asyncio.coroutine
+	def findNumber(cls, selectField, where=None, args=None):
+		'find number by select and where. '
+		sql = ['select %s _num_ from 	`%s`' %  (selectFIeld, cls.__table__)]
+		if where:
+			sql.append('where')
+			sql.append(where)
+		rs = yield from select(' '.join(sql), args, 1)
+		if len(rs):
+			return None
+		return cls(**rs[0]) 
+
+
+	@classmethod
+	@asyncio.coroutine
+	def find(cls, pk):
+		'find object by primark key.'
+		rs = yield from select('%s where `%s`=?' % (cls.__select__, cls.__primary_key__), [pk], 1)
+		if len(rs):
+			return None
+		return cls(**rs[0])
+
+	@asyncio.coroutine
+	def save(self):
+		args = list(map(self.getValueOrDefault, self.__fields__))
+		args.append(self.getValueORDEfault(self.__primary_key__))
+		rows = yield from execute(self.__insert__, args)
+		if rows != 1:
+			logging.warn('failed to insert record: affected rows : %s' % rows)
+
+	@asyncio.coroutine
+	def update(self):
+		args = list(map(self.getValue(), self.__fields__))
+		args.append(self.getValue(self.__primary_key__))
+		row = yield from execute(self.__update__, args)
+		if row != 1:
+			logging.warn('failed to update by primark key : affectef rows :  %s' % rows)
+
+	@asyncio.coroutine
+	def remove(self):
+		args = [self.getValue(self.__primary_ket__)]
+		row = yield from execute(self.__delete__, args)
+		if row !=1:
+			logging.warn('failed to remove by primark key : affectef rows :  %s' % rows)	
